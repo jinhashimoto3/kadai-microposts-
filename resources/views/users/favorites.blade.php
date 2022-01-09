a9599fabd623b378a31a7122646c9f7cf48c797f
jinhashimoto3 <h.jin.6621@gmail.com>
2022-01-09 11:06:32 +0000

    microposts

--- resources/views/users/favorites.blade.php
+++ resources/views/users/favorites.blade.php
@@ -1,24 +1,31 @@
 @extends('layouts.app')
 @section('content')
  <h1>お気に入り一覧</h1>
+ @if (count($favorites) > 0)
+    <ul class="list-unstyled">
+        @foreach ($favorites as $favorite)
+            <li class="media mb-3">
+                {{-- 投稿の所有者のメールアドレスをもとにGravatarを取得して表示 --}}
+                <img class="mr-2 rounded" src="{{ Gravatar::get($favorite->user->email, ['size' => 50]) }}" alt="">
+                <div class="media-body">
+                    <div>
+                        {{-- 投稿の所有者のユーザ詳細ページへのリンク --}}
+                        {!! link_to_route('users.show', $favorite->user->name, ['user' => $favorite->user->id]) !!}
+                        <span class="text-muted">posted at {{ $favorite->created_at }}</span>
+                    </div>
+                    <div>
+                        {{-- 投稿内容 --}}
+                        <p class="mb-0">{!! nl2br(e($favorite->content)) !!}</p>
+                    </div>
+                </div>
+            </li>
+        @endforeach
+    </ul>
+    {{-- ページネーションのリンク --}}
+    {{ $favorites->links() }}
+@endif
 
-    @if (count($favorites) > 0)
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>投稿</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($favorites as $favorite)
-                <tr>
-                    <td>{{$favorite->content}}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-    @endif
-
+  
 
 
 
